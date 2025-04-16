178b6e05d9fc4dec914e432b28c455af902dd595
5647kr <5647kr@gmail.com>
2025-04-16 22:05:25 +0900

    2025.04.16 cart 페이지(숙제)

---
+++ 2025.04.16/app/cart/layout.js
@@ -0,0 +1,8 @@
+export default function Layout({ children }) {
+  return (
+    <div>
+      {/* <p>현대카드 무이자이벤트 중</p> */}
+      {children}
+    </div>
+  )
+}
---
+++ 2025.04.16/app/cart/page.js
@@ -0,0 +1,19 @@
+export default function page() {
+  return (
+    <div>
+      <h1 className="title">장바구니</h1>
+      <ul>
+        <li className="cart-item">
+          <p>상품명</p>
+          <p>$40</p>
+          <p>1개</p>
+        </li>
+        <li className="cart-item">
+          <p>상품명</p>
+          <p>$40</p>
+          <p>1개</p>
+        </li>
+      </ul>
+    </div>
+  )
+}
