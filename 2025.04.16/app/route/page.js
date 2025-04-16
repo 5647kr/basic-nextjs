e1b0c74eac2fa8e1a7c85aab9b06ef047a9574ec
5647kr <5647kr@gmail.com>
2025-04-16 22:06:02 +0900

    2025.04.16 라우터 설정 방법

---
+++ 2025.04.16/app/route/page.js
@@ -0,0 +1,9 @@
+export default function Route() {
+  return (
+    <div>
+      <h1>라우팅</h1>
+      <p>app폴더 안에 폴더 생성 후 page.js 생성한다.</p>
+      <p>만일 메뉴바, footer, header같이 공통 컴포넌트의 경우 layout.js 파일 안에 작성한다.</p>
+    </div>
+  )
+}
