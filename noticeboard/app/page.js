daab332d99ecd4e4fc64ebab1d38e5529b2d8031
5647kr <5647kr@gmail.com>
2025-04-18 22:02:19 +0900

    noticeboard 초기 설정

--- noticeboard/app/page.js
+++ noticeboard/app/page.js
@@ -1,7 +1,14 @@
-export default function Home() {
+// import { connectDB } from "@/util/database.js"
+
+
+export default async function Home() {
+  // const db = (await connectDB).db("noticeboard");
+
+  // let result = await db.collection("post").find().toArray();
+  
   return (
     <div>
-
+      안녕
     </div>
   );
 }
