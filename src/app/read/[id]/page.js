caf0720c72a84dffcbd6e713a0f66f2838d7a9dd
5647kr <5647kr@gmail.com>
2025-05-07 16:14:54 +0900

    ch8_통신 데이터 가져오기

--- src/app/read/[id]/page.js
+++ src/app/read/[id]/page.js
@@ -1,9 +1,13 @@
-export default function Read(props) {
+export default async function Read(props) {
+  const response = await fetch(`http://localhost:9999/topics/${props.params.id}`);
+  const topic = await response.json();
+
   return (
     <>
       {/* 다이나믹 라우팅 */}
-      <h2>read</h2>
+      <h2>{topic.title}</h2>
       {props.params.id}
+      {topic.body}
     </>
   )
 }
