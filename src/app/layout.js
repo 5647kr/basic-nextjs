17a1995217895ddf58c19f7d07e5abdec2cc1d7d
5647kr <5647kr@gmail.com>
2025-05-07 16:14:00 +0900

    ch4_싱글페이지 & ch7_backend와 통신

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,19 +1,55 @@
+// "use client"
+import Link from "next/link";
 import "./globals.css";
+// import { useEffect, useState } from "react";
 
-export default function RootLayout({ children }) {
+export default async function RootLayout({ children }) {
+  // const [topics, setTopics] = useState([]);
+  // useEffect(() => {
+  //   fetch("http://localhost:9999/topics")
+  //   .then(response => response.json())
+  //   .then(result => {
+  //     setTopics(result)
+  //   });
+  // }, [])
+
+  const response = await fetch("http://localhost:9999/topics");
+  const topics = await response.json();
+  
   return (
     <html>
       <body>
-        <h1><a href="/">WEB!</a></h1>
+        <h1>
+          <Link href="/">WEB!</Link>
+        </h1>
         <ol>
-          <li><a href="/read/1">html</a></li>
-          <li><a href="/read/2">css</a></li>
+          {topics.map((topic) => {
+            return (
+              <li key={topic.id}>
+                <Link href={`/read/${topic.id}`}>{topic.title}
+                </Link>
+              </li>
+            )
+          })}
+          {/* <li>
+            <Link href="/read/1">html</Link>
+          </li>
+          <li>
+            <Link href="/read/2">css</Link>
+          </li> */}
         </ol>
         {children}
         <ul>
-          <li><a href="/create">Create</a></li>
-          <li><a href="/update/1">Update</a></li>
-          <li><input type="button" value="delete"/></li>
+          <li>
+            <Link href="/create">Create</Link>
+          </li>
+          <li>
+            <Link href="/update/1">Update
+            </Link>
+          </li>
+          <li>
+            <input type="button" value="delete" />
+          </li>
         </ul>
       </body>
     </html>
