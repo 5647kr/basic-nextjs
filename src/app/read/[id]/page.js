bfce482ed89efaa89d37d7f6fd13d313279abb2b
5647kr <5647kr@gmail.com>
2025-05-08 15:01:16 +0900

    ch11_환경변수

--- src/app/read/[id]/page.js
+++ src/app/read/[id]/page.js
@@ -1,12 +1,13 @@
 export default async function Read(props) {
-  const response = await fetch(`http://localhost:9999/topics/${props.params.id}`);
+  const id = props.params.id;
+  const response = await fetch(process.env.NEXT_PUBLIC_API_URL + `topics/${id}`, {cache: "no-store"});
   const topic = await response.json();
 
   return (
     <>
       {/* 다이나믹 라우팅 */}
       <h2>{topic.title}</h2>
-      {props.params.id}
+      {/* {props.params.id} */}
       {topic.body}
     </>
   )
