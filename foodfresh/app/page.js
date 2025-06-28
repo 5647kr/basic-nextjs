b9a243b66eb58e1944385a49515208f768c92cde
5647kr <5647kr@gmail.com>
2025-06-28 22:59:55 +0900

    foodfresh 프로젝트 학습[#2 className, 변수, style 속성 추가]

--- foodfresh/app/page.js
+++ foodfresh/app/page.js
@@ -1,7 +1,13 @@
+import "./globals.css";
+
 export default function Home() {
+  let name = "park";
+  // let link = "https://google.com"
   return (
     <div>
-      
+      <h4 className="title">애플후레시</h4>
+      <p className="title-sub">by dev {name}</p>
+      {/* <a href={link}>링크</a> */}
     </div>
   );
 }
