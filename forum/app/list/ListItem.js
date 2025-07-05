5369618ecf8023e2420b6a65bcf62a253f2c9bae
5647kr <5647kr@gmail.com>
2025-07-05 15:10:23 +0900

    forum 프로젝트 학습[#3 몽고db로 글목록 조회기능 만들기]

--- forum/app/list/ListItem.js
+++ forum/app/list/ListItem.js
@@ -14,7 +14,36 @@ export default function ListItem({ result }) {
               <p>1월 1일</p>
             </Link>
             <Link href={`/edit/${item._id}`}>수정</Link>
-            <button>삭제</button>
+
+            {/* fetch 방법1 */}
+            {/* <span onClick={(e) => {
+              fetch("/api/post/delete", {
+                method: "DELETE",
+                body: item._id
+              }).then((r) => r.json())
+              .then(() => {
+                e.target.parentElement.style.opacity = 0;
+                setTimeout(() => {
+                  e.target.parentElement.style.display = "none";
+                }, 1000)
+              })
+            }}>삭제</span> */}
+
+            {/* fetch 방법2 */}
+            <span onClick={(e) => {
+              fetch("api/test?name=kim&age=20")
+            }}>삭제</span>
+
+
+
+
+
+
+
+
+
+
+
             {/* <DetailLink /> */}
           </li>
         );
--- forum/app/list/page.js
+++ forum/app/list/page.js
@@ -8,43 +8,59 @@ import ListItem from "./ListItem";
 export default async function List() {
   const db = (await connectDB).db("forum");
   const result = await db.collection("post").find().toArray();
-  console.log(result);
-
-    const serializedResult = result.map(item => ({
-    ...item,
-    _id: item._id.toString() // ObjectId를 문자열로 변환
-  }));
 
   return (
     <ul className="list-bg">
-      <ListItem result={serializedResult}/>
-
-
-
-      {/* <li className="list-item">
-        <h4>글 제목</h4>
-        <p>1월 1일</p>
-      </li>
-      <li className="list-item">
-        <h4>글 제목</h4>
-        <p>1월 1일</p>
-      </li> */}
+      {result.map((item) => {
+        console.log(item._id)
+        return (
+          <li className="list-item" key={item._id}>
+            <Link href={`/detail/${item._id.toString()}`}>
+              <h4>{item.title}</h4>
+              <p>{item.content}</p>
+              <p>1월 1일</p>
+            </Link>
+            <Link href={`/edit/${item._id.toString()}`}>수정</Link>
+          </li>
+        );
+      })}
     </ul>
   );
 }
 
 
-      // {result.map((item) => {
-      //   console.log(item._id)
-      //   return (
-      //     <li className="list-item" key={item._id}>
-      //       <Link href={`detail/${item._id}`}>
-      //         <h4>{item.title}</h4>
-      //         <p>{item.content}</p>
-      //         <p>1월 1일</p>
-      //       </Link>
-      //       <Link href={`/edit/${item._id}`}>수정</Link>
-      //       {/* <DetailLink /> */}
-      //     </li>
-      //   );
-      // })}
+  // 글 목록 조회 기능1
+  // return (
+  //   <ul className="list-bg">
+  //     <li className="list-item">
+  //       <h4>글 제목</h4>
+  //       <p>1월 1일</p>
+  //     </li>
+  //     <li className="list-item">
+  //       <h4>글 제목</h4>
+  //       <p>1월 1일</p>
+  //     </li>
+  //   </ul>
+  // );
+
+
+  // 글 목록 조회 기능2
+  // return (
+  //   <ul className="list-bg">
+  //     {result.map((item) => {
+  //       console.log(item._id)
+  //       return (
+  //         <li className="list-item" key={item._id}>
+  //           <Link href={`/detail/${item._id}`}>
+  //             <h4>{item.title}</h4>
+  //             <p>{item.content}</p>
+  //             <p>1월 1일</p>
+  //           </Link>
+  //           <Link href={`/edit/${item._id}`}>수정</Link>
+  //         </li>
+  //       );
+  //     })}
+  //   </ul>
+  // );
+
+
