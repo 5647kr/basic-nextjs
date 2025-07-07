fdd69d327d58aa8ca2a5a97ba26164d906b8d4c8
5647kr <5647kr@gmail.com>
2025-07-07 10:01:21 +0900

    forum 프로젝트 학습[#9 삭제 기능 구현]

--- forum/pages/api/post/delete.js
+++ forum/pages/api/post/delete.js
@@ -3,12 +3,10 @@ import { ObjectId } from "mongodb";
 
 export default async function handler(req, res) {
   if (req.method === "DELETE") {
-    console.log(req)
     const db = (await connectDB).db("forum");
     let result = await db
       .collection("post")
       .deleteOne({ _id: new ObjectId(req.body) });
-    console.log(result);
     res.status(200).json("삭제완료");
   }
 }
