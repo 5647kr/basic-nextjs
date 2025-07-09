748a6b30d36e2577a91a63878ce62d260b045bf7
5647kr <5647kr@gmail.com>
2025-07-10 08:27:53 +0900

    forum 프로젝트 학습[#10 dynamic rendering]

--- forum/app/list/page.js
+++ forum/app/list/page.js
@@ -4,10 +4,30 @@ import ListItem from "./ListItem";
 // import DetailLink from "./DetailLink";
 
 
+// 해당 코드는 static rendering을 dynamic rendering으로 바꿔주는 코드이다.
+export const dynamic = "force-dynamic" 
+
+// dynamic 단점: 매번 새로 html페이지를 그려야 해서 서버/DB 부담이 커진다. 이를 캐싱기능을 사용해 보안할 수 있다.
+
+// 캐싱이란 결과를 잠깐 저장해두고 재사용하는것을 의미한다.
+// 페이지 캐싱이란 페이지 완성본을 잠깐 저장해두고 재사용
+
+// 해당 코드는 dynamic rendering을 static rendering으로 바꿔주는 코드이다.
+// export const dynamic = "force-static"
+
+// 이 코드를 사용시 해당 파일을 60초마다 캐싱하는 기능을 사용할 수 있게 도와준다.
+// export const revalidate = 60;
+
+
 
 export default async function List() {
   const db = (await connectDB).db("forum");
   const data = await db.collection("post").find().toArray();
+
+  // Get 요청 결과를 캐싱하여 서버 부담을 줄여본다.
+  // 아래 두가지 방법 모두 캐싱으로 사용 가능하다.
+  // 1. await fetch("/url", {cache: "force-cache"})
+  // 2. await fetch("/url")
 
   const result = data.map((item) => ({
     ...item,
